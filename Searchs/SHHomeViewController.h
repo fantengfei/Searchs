7bf6c937b78a870478631b39f00e91a59de2e9db
taffy <taffy@zhihu.com>
2016-02-22 16:15:12 +0800

    app icon

---
+++ Searchs/SHHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  SHHomeViewController.h
+//  Searchs
+//
+//  Created by taffy on 16/2/22.
+//  Copyright © 2016年 taffy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHHomeViewController : UIViewController
+
+@end
---
+++ Searchs/SHHomeViewController.m
@@ -0,0 +1,18 @@
+//
+//  SHHomeViewController.m
+//  Searchs
+//
+//  Created by taffy on 16/2/22.
+//  Copyright © 2016年 taffy. All rights reserved.
+//
+
+#import "SHHomeViewController.h"
+
+@implementation SHHomeViewController
+
+- (void) viewDidLoad {
+  [super viewDidLoad];
+  
+}
+
+@end
